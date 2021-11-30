457a5443d55d2c99990be5dddc63524ff3247332
Ariel Gluzman <52013867+arielgluzman2003@users.noreply.github.com>
2021-11-30 21:56:01 +0200

    added use case of newly implemented "outDegree"

--- useExample.c
+++ useExample.c
@@ -36,5 +36,11 @@ int main()
 	deleteEdge(&G, vertices, &V[0], &V[1]);
 
 	/* Show Difference after deleting edge (A,B) */
-	printf("\n%s", isEdge(&G, vertices, &V[0], &V[1]) == 1 ? "Edge (A,B) is in E" : "Edge (A,B) is NOT in E");
+	printf("\n%s\n\n", isEdge(&G, vertices, &V[0], &V[1]) == 1 ? "Edge (A,B) is in E" : "Edge (A,B) is NOT in E");
+
+	/* Print Current State of Graph G*/
+	printGraph(&G, vertices);
+
+	/* Print out-degree of Vertex V[0] ('A') */
+	printf("\nOut-Degree of vertex '%c': %d\n", V[0].value, outDegree(&G, vertices, &V[0]));
 }
